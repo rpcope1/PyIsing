3b0f1797092d5fa1db37a39056164103e2642dc1
rpcope1 <rpcope1@gmail.com>
2013-07-20 16:59:44 -0400

    Updated the GUI.

--- ising_gui.py
+++ ising_gui.py
@@ -1,7 +1,87 @@
-from TKinter import ALL, Canvas, Frame, SUNKEN, Tk
+from Tkinter import *
+import colorsys
 
 class ising_gui(object):
-    PIXEL_HEIGHT = 10
+    MIN_RED = MIN_GREEN = MIN_BLUE = 0x0
+    MAX_RED = MAX_GREEN = MAX_BLUE = 0xFF    
     PIXEL_WIDTH = 10
 
     def __init__(self, grid_x, grid_y):
+        self.grid_x = grid_x
+        self.grid_y = grid_y
+        self.pixels = [(0, 0, 0) for i in range(self.grid_x * self.grid_y)]
+        
+
+        #Set up TK
+        self.root = Tk()
+        self.root.title("Ising Simulation %d x %d" % (self.grid_x, self.grid_y))
+        self.root.resizable(0, 0)
+        #Set up the frame for holding the Ising Simulation results.
+        self.frame = Frame(self.root, bd=5, relief=SUNKEN)
+        self.frame.pack(side = TOP)
+
+        self.canvas = Canvas(self.frame,
+                             width=self.PIXEL_WIDTH * self.grid_x,
+                             height=self.PIXEL_WIDTH * self.grid_y,
+                             bd=0, highlightthickness=0)
+        self.canvas.pack()
+
+        
+        #Put Boxes here...
+        self.TString = StringVar()
+        self.EString = StringVar()
+        self.GString = StringVar()    
+        
+        self.bframe = Frame(self.root)
+        self.TLabel = Label(self.bframe, anchor=N, textvariable = self.TString)
+        self.ELabel = Label(self.bframe, anchor=S, textvariable = self.EString)
+        self.GLabel = Label(self.bframe, anchor=CENTER, textvariable = self.GString)   
+        self.bframe.pack(side = BOTTOM)          
+        self.root.update()
+    
+    def set_pixel(self, x, y, hsv):
+        self.pixels[self.grid_x * y + x] = hsv
+
+    def get_pixel(self, x, y):
+        return self.pixels[self.grid_x * y + x]
+
+    def set_temperature(self, temp):
+        self.TString.set("Temperature: %f" % temp)
+        self.TLabel.pack()
+
+    def set_energy(self, energ):
+        self.EString.set("Energy: %f" % energ)
+        self.ELabel.pack()
+
+    def set_correlation(self, cfunc):
+        self.GString.set("Correlation: %f" % cfunc)
+        self.GLabel.pack()
+    
+    def draw(self):
+        self.canvas.delete(ALL)
+        for x in range(len(self.pixels)):
+            x_0 = (x % self.grid_x) * self.PIXEL_WIDTH
+            y_0 = (x / self.grid_x) * self.PIXEL_WIDTH
+            x_1 = x_0 + self.PIXEL_WIDTH
+            y_1 = y_0 + self.PIXEL_WIDTH
+            hue = "#%02x%02x%02x" % self._get_rgb(self.pixels[x])
+            self.canvas.create_rectangle(x_0, y_0, x_1, y_1, fill=hue)
+        self.canvas.update()
+
+    def clear(self):
+        for i in range(self.width * self.height):
+            self.pixels[i] = (0, 0, 0)
+
+    def _hsv_to_rgb(self, hsv):
+        rgb = colorsys.hsv_to_rgb(*hsv)
+        red = self.MAX_RED * rgb[0]
+        green = self.MAX_GREEN * rgb[1]
+        blue = self.MAX_BLUE * rgb[2]
+        return (red, green, blue)
+
+    def _get_rgb(self, hsv):
+        red, green, blue = self._hsv_to_rgb(hsv)
+        red = int(float(red) / (self.MAX_RED - self.MIN_RED) * 0xFF)
+        green = int(float(green) / (self.MAX_GREEN - self.MIN_GREEN) * 0xFF)
+        blue = int(float(blue) / (self.MAX_BLUE - self.MIN_BLUE) * 0xFF)
+        return (red, green, blue)
--- ising_main.py
+++ ising_main.py
@@ -4,52 +4,61 @@
 
 from ising_grid import *
 #from ising_gui import *
-from wall import Wall
+from ising_gui import ising_gui
 import time
 import pylab
 
-def set_values(igrid, wall, x_dim, y_dim):
+def set_values(igrid, disp, x_dim, y_dim):
     up = (0, 0, 0x00)
     down = (0, 0,0xFF)
+    retv = (igrid.get_T(), igrid.get_E_total(), igrid.get_G_avg())
+    disp.set_temperature(retv[0])
+    disp.set_energy(retv[1])
+    disp.set_correlation(retv[2])
+
     for j in range(y_dim):
             for i in range(x_dim):
                 value = igrid.get_val(i, j)
                 if(value == 1):
-                    wall.set_pixel(i, j, (0, 0, 0x00))
+                    disp.set_pixel(i, j, (0, 0, 0x00))
                 elif(value == -1):
-                    wall.set_pixel(i, j, (0, 0, 0xFF))
+                    disp.set_pixel(i, j, (0, 0, 0xFF))
                 else:
-                    wall.set_pixel(i, j, (0x00, 0xFF, 0xFF))
+                    disp.set_pixel(i, j, (0x00, 0xFF, 0xFF))
 
+    return retv
 
 if __name__ == "__main__":
     x_dim = 40
     y_dim = 40
     grid = ising_grid(x_dim, y_dim)
-    T_start = 7.6
-    T_end = 0.01
-    grid.set_T(T_start)
-    grid.set_B(0.00)
-    grid.set_J(1.00)
-    disp = Wall(x_dim, y_dim)
-    itr = 0
-    itr_max = 250
-    grid.randomize()
+    disp = ising_gui(x_dim, y_dim)    
+
     listT = []
     listE = []
     listG = []
+    
+    T_start = 3.0
+    T_end = 1.0
+    T = T_start
+    grid.set_T(T_start)
+    grid.set_B(0.00)
+    grid.set_J(1.00)
+    
+    itr = 0
+    itr_max = 250
+    grid.randomize()
+    #We're going to run a simulated annealing.
     while(itr < itr_max):
-        set_values(grid, disp, x_dim, y_dim)
+        (T, E, G) = set_values(grid, disp, x_dim, y_dim)
         disp.draw()
         monte_carlo_update(grid)
-        T = T_start - (itr/(1.0*itr_max))*(T_start-T_end)
-        grid.set_T(T)
-        E = grid.get_E_total()
-        G = grid.get_G_avg()        
         print "Temp = ", T, "Total Energy = ", E, "Order parameter = ", G
         listE.append(E)
         listT.append(T)
-        listG.append(G)        
+        listG.append(G)
+        T = T_start - (itr/(1.0*itr_max))*(T_start-T_end)
+        grid.set_T(T)        
         itr += 1
         time.sleep(0.05)
 
